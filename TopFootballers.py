0355326b2a62bbff4f8b53a58b854353b1cbf560
pawelkabat94 <115871494+pawelkabat94@users.noreply.github.com>
2024-03-24 21:03:36 +0100

    Delete TopFootballers.py

--- TopFootballers.py
+++
@@ -1,4 +0,0 @@
-footballers = ("Messi", "Ronaldo", "Pele", "Maradona", "Cruyff", "Beckenbauer", "Zidane", "Iniesta", "Platini", "Di Stefano")
-
-for mytop10 in footballers:
-    print (f"{mytop10} is in my top10 greatest footballes ever")
